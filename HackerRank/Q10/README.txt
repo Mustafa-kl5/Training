aa36947a276d54d0c1b08139c923da985d1bd2ff
Mustafa Mahmood <mmalowisi19@cit.just.edu.jo>
2022-06-23 18:49:25 +0300

    solve more

--- HackerRank/Q10/README.txt
+++ HackerRank/Q10/README.txt
@@ -1,1 +1,1 @@
-Create a function that takes the age in years and returns the age in days
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
--- HackerRank/Q10/javaScript.js
+++ HackerRank/Q10/javaScript.js
@@ -1,5 +1,17 @@
-function calcAge(age) {
-    return age * 365;
+function isIsogram(str) {
+    var i, j;
+    str = str.toLowerCase();
+    for (i = 0; i < str.length; ++i)
+        for (j = i + 1; j < str.length; ++j)
+            if (str[i] === str[j]) return false;
+    return true;
 }
+
 let tag1 = document.getElementById("tag1");
-tag1.innerHTML = calcAge(65);
+let text = "mustafe";
+let text2 = "mustafa";
+tag1.innerHTML +=
+    "test with no repeat char in text => " +
+    isIsogram(text) +
+    "\n test with repeat char in text => " +
+    isIsogram(text2);
